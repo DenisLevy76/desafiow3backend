d9924d69929f4df3e138480c2381ac4f7e664e70
Denis Levy <denis.levi11@gmail.com>
2022-05-09 18:21:12 -0300

    015 - ...

---
+++ src/core/application/useCases/GetAccountAndValidateUseCase.ts
@@ -0,0 +1,38 @@
+import { Account } from '../../domain/entities/Account';
+import { IAccountRepository } from '../repositories/IAccount.repository';
+import { IBankOfficeRepository } from '../repositories/IBankOffice.repository';
+import { IClientRepository } from '../repositories/IClient.repository';
+
+export class GetAccountAndValidateUseCase {
+  constructor(
+    private accountRepo: IAccountRepository,
+    private bankOfficeRepo: IBankOfficeRepository,
+    private clientRepo: IClientRepository,
+  ) {}
+
+  async execute(
+    accountBankOfficeNumber: string,
+    accountNumber: string,
+  ): Promise<Account> {
+    const bankOfficeDto = await this.bankOfficeRepo.findByNumber(
+      accountBankOfficeNumber,
+    );
+
+    if (!bankOfficeDto) throw new Error('Bank office does not exist.');
+
+    const accountDto = await this.accountRepo.findByBranchAndNumber(
+      bankOfficeDto.number,
+      accountNumber,
+    );
+
+    if (!accountDto) throw new Error('Account does not exist.');
+
+    const clientDto = await this.clientRepo.findById(accountDto.clientId);
+
+    if (!clientDto) throw new Error('Client does not exist.');
+
+    const account = Account.createByDto(accountDto, clientDto, bankOfficeDto);
+
+    return account;
+  }
+}
